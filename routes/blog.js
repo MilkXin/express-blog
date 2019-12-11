cbe3e21b8cd22a45287f420656bd70a8ed26ede4
chen xin <729609320@.com>
2019-12-11 22:02:15 +0800

    连接数据库，打通查询博客列表接口

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const { getList, getDetail, newBlog, updateBlogData, delBlog } = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.get('/list', function(req, res, next) {
-    res.json({
-        errno: 0,
-        data: [1, 2, 3]
-    })
+    const author = req.query.author || ''
+        const keyword = req.query.keyword || ''
+        // const listData = getList(author, keyword)
+
+        // return new SuccessModel(listData)
+
+        const result = getList(author, keyword)
+        return result.then(listData => {
+            res.json(
+                new SuccessModel(listData)
+            )
+        })
 })
 
 module.exports = router
